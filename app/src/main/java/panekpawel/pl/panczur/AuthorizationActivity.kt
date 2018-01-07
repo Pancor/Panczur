350cd48caeda5be2285deee7db2cd29ad5802421
Pancor <klawisz338@gmail.com>
2018-01-07 19:27:01 +0100

    Initial commit

---
+++ app/src/main/java/panekpawel/pl/panczur/AuthorizationActivity.kt
@@ -0,0 +1,12 @@
+package panekpawel.pl.panczur
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorizationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_authorization)
+    }
+}
