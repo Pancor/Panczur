6c7b4e1dacab34c9e6faba49e318cb1fd09eee7c
Pancor <klawisz338@gmail.com>
2018-01-29 11:54:16 +0100

    Added crashlytics

--- app/src/main/java/panekpawel/pl/panczur/utils/logger/Logger.kt
+++ app/src/main/java/panekpawel/pl/panczur/utils/logger/Logger.kt
@@ -1,6 +1,7 @@
 package panekpawel.pl.panczur.utils.logger
 
 import android.util.Log
+import com.crashlytics.android.Crashlytics
 import panekpawel.pl.panczur.BuildConfig
 import timber.log.Timber
 import javax.inject.Inject
@@ -20,9 +21,15 @@ class Logger @Inject constructor(){
     private class CrashReportingTree : Timber.Tree() {
 
         override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
-           if (priority == Log.VERBOSE || priority == Log.DEBUG) {
-               return
-           }
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            }
+            Crashlytics.log(priority, tag, message)
+            if (t != null) {
+                if (priority == Log.ERROR || priority == Log.WARN) {
+                    Crashlytics.logException(t)
+                }
+            }
         }
     }
 }
