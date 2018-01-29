227a6b20c66b784e2355d5d9d37c8de2c3e6e243
Pancor <klawisz338@gmail.com>
2018-01-29 11:15:46 +0100

    Build base of logger

--- app/src/main/java/panekpawel/pl/panczur/utils/logger/Logger.kt
+++ app/src/main/java/panekpawel/pl/panczur/utils/logger/Logger.kt
@@ -1,9 +1,28 @@
 package panekpawel.pl.panczur.utils.logger
 
+import android.util.Log
+import panekpawel.pl.panczur.BuildConfig
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class Logger @Inject constructor(){
 
+    init {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
+        }
+    }
+
+    private class CrashReportingTree : Timber.Tree() {
+
+        override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
+           if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+               return
+           }
+        }
+    }
 }
