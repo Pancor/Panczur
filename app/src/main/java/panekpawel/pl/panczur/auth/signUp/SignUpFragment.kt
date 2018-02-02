1f92f8424590acb91433642c2f8ec790e258af75
Pancor <klawisz338@gmail.com>
2018-02-02 18:36:32 +0100

    Added base structure of authorization module

--- app/src/main/java/panekpawel/pl/panczur/auth/signUp/SignUpFragment.kt
+++ app/src/main/java/panekpawel/pl/panczur/auth/signUp/SignUpFragment.kt
@@ -1,7 +1,16 @@
 package panekpawel.pl.panczur.auth.signUp
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import panekpawel.pl.panczur.R
 
 
 class SignUpFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?):
+            View? = inflater.inflate(R.layout.fr_sign_up, container, false)
 }
