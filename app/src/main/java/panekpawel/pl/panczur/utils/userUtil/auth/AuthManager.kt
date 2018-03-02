6590fd6b32f99353df3bfe487768c92d192cecd5
pancor <pancordev@gmail.com>
2018-03-02 18:48:29 +0100

    Added tests

--- app/src/main/java/panekpawel/pl/panczur/utils/userUtil/auth/AuthManager.kt
+++ app/src/main/java/panekpawel/pl/panczur/utils/userUtil/auth/AuthManager.kt
@@ -1,20 +1,15 @@
 package panekpawel.pl.panczur.utils.userUtil.auth
 
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
-import io.reactivex.Emitter
 import io.reactivex.Single
-import io.reactivex.SingleEmitter
-import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Cancellable
 import panekpawel.pl.panczur.models.Result
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AuthManager @Inject constructor(val fireBaseAuth: FirebaseAuth) : AuthContract {
+class AuthManager @Inject constructor(private val fireBaseAuth: FirebaseAuth) : AuthContract {
 
     companion object {
         const val SIGN_IN_SUCCEED = "SIGN_IN_SUCCEED"
--- app/src/test/java/panekpawel/pl/panczur/utils/userUtil/auth/AuthManagerTest.kt
+++ app/src/test/java/panekpawel/pl/panczur/utils/userUtil/auth/AuthManagerTest.kt
@@ -4,6 +4,7 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers
@@ -11,22 +12,19 @@ import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.Mockito.`when` as whenever
 import org.mockito.MockitoAnnotations
-import org.mockito.invocation.InvocationOnMock
-import org.mockito.stubbing.Answer
 import panekpawel.pl.panczur.models.Result
 
 class AuthManagerTest {
 
-    @Mock
-    private lateinit var fireBaseAuth: FirebaseAuth
-
-    @Mock
-    private lateinit var authResult: Task<AuthResult>
-
     private lateinit var authManager: AuthManager
+
+    @Mock private lateinit var fireBaseAuth: FirebaseAuth
+    @Mock private lateinit var authResult: Task<AuthResult>
+    @Mock private lateinit var exception: FirebaseAuthException
 
     private val EMAIL = "email@email.email"
     private val PASSWORD = "password"
+    private val ERROR_CODE = "error_code"
 
     @Before
     fun setupAuthManager() {
@@ -35,17 +33,11 @@ class AuthManagerTest {
     }
 
     @Test
+    @Suppress("UNCHECKED_CAST")
     fun signInByEmailAndPasswordWithSuccessThenCheckIfResultSucceeds() {
         whenever(authResult.isSuccessful).thenReturn(true)
         whenever(fireBaseAuth.signInWithEmailAndPassword(EMAIL, PASSWORD)).thenReturn(authResult)
-
-        doAnswer {
-            val listener = it.arguments[0] as OnCompleteListener<AuthResult>
-            listener.onComplete(authResult)
-            authResult
-        }.`when`(authResult)
-                .addOnCompleteListener(ArgumentMatchers.any<OnCompleteListener<AuthResult>>())
-
+        callOnCompleteOfAuthResult()
         val expectedResult = Result(isSucceed = true, code = AuthManager.SIGN_IN_SUCCEED)
 
         val testObserver = authManager.signIn(EMAIL, PASSWORD)
@@ -55,5 +47,50 @@ class AuthManagerTest {
         testObserver
                 .assertNoErrors()
                 .assertValue(expectedResult)
+    }
+
+    @Test
+    fun signInByEmailAndPasswordWithFailThenCheckIfResultFailsWithNull() {
+        whenever(authResult.isSuccessful).thenReturn(false)
+        whenever(authResult.exception).thenReturn(null)
+        whenever(fireBaseAuth.signInWithEmailAndPassword(EMAIL, PASSWORD)).thenReturn(authResult)
+        callOnCompleteOfAuthResult()
+        val expectedResult = Result(isSucceed = false, code = AuthManager.UNKNOWN_ERROR)
+
+        val testObserver = authManager.signIn(EMAIL, PASSWORD)
+                .test()
+        testObserver.awaitTerminalEvent()
+
+        testObserver
+                .assertNoErrors()
+                .assertValue(expectedResult)
+    }
+
+    @Test
+    fun signInByEmailAndPasswordWithFailThenCheckIfResultFailsWithException() {
+        whenever(authResult.isSuccessful).thenReturn(false)
+        whenever(authResult.exception).thenReturn(exception)
+        whenever(exception.errorCode).thenReturn(ERROR_CODE)
+        whenever(fireBaseAuth.signInWithEmailAndPassword(EMAIL, PASSWORD)).thenReturn(authResult)
+        callOnCompleteOfAuthResult()
+        val expectedResult = Result(isSucceed = false, code = ERROR_CODE)
+
+        val testObserver = authManager.signIn(EMAIL, PASSWORD)
+                .test()
+        testObserver.awaitTerminalEvent()
+
+        testObserver
+                .assertNoErrors()
+                .assertValue(expectedResult)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun callOnCompleteOfAuthResult() {
+        doAnswer {
+            val listener = it.arguments[0] as OnCompleteListener<AuthResult>
+            listener.onComplete(authResult)
+            authResult
+        }.`when`(authResult)
+                .addOnCompleteListener(ArgumentMatchers.any<OnCompleteListener<AuthResult>>())
     }
 }
